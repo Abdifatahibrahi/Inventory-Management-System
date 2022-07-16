b1fc48ba5f429d433514fc603b20f78755b5d99e
Abdifatah Ibrahim <alsidiq099@gmail.com>
2022-07-16 06:56:26 +0300

    Created The admin dashboard

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from .models import Product
+from django.contrib.auth.models import Group
+
+admin.site.site_header = 'AbdifatahInventory Dashboard'
 
 
-
-admin.site.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'quantity',)
+    list_filter = ('category',)
+admin.site.register(Product, ProductAdmin)
