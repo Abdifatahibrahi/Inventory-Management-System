e9f6441eb89666c3df11d18bda7bc89e1e84a904
Abdifatah Ibrahim <alsidiq099@gmail.com>
2022-07-17 10:15:35 +0300

    Added an Order model that has relationship with products and staff

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, Order
 from django.contrib.auth.models import Group
 
 admin.site.site_header = 'AbdifatahInventory Dashboard'
@@ -8,4 +8,7 @@ admin.site.site_header = 'AbdifatahInventory Dashboard'
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('name', 'category', 'quantity',)
     list_filter = ('category',)
+
+
 admin.site.register(Product, ProductAdmin)
+admin.site.register(Order)
