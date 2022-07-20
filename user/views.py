e564d584d848a761809ca6c28eba485979fdac96
Abdifatah Ibrahim <alsidiq099@gmail.com>
2022-07-20 10:26:01 +0300

    Created user registration form

--- user/views.py
+++ user/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from .forms import CreateUserForm
 
 def register(request):
 
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = CreateUserForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('dashboard-index')
     else:
-        form = UserCreationForm()
+        form = CreateUserForm()
     context = {
         'form': form 
     }
