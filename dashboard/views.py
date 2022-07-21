5a21ac5b7b60a863463925b13fbdd7fe7852164f
Abdifatah Ibrahim <alsidiq099@gmail.com>
2022-07-21 07:37:05 +0300

    Added login_required decorators and also added the logic to differentiate the dashboard for the normal users and the staff

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,16 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
 
+@login_required(login_url='user-login')
 def index(request):
     return render(request, 'dashboard/index.html')
 
+@login_required(login_url='user-login')
 def staff(request):
     return render(request, 'dashboard/staff.html')
 
-
+@login_required(login_url='user-login')
 def product(request):
     return render(request, 'dashboard/product.html')
 
+@login_required(login_url='user-login')
 def order(request):
     return render(request, 'dashboard/order.html')
