699012b45ae8d4f0bf57d510799876885df6235b
Abdifatah Ibrahim <alsidiq099@gmail.com>
2022-07-25 17:55:23 +0300

    Added crud functionalities

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('dashboard', views.index, name='dashboard-index'),
     path('staff/', views.staff, name='dashboard-staff'),
     path('product/', views.product, name='dashboard-product'),
+    path('product/delete/<int:pk>', views.delete_product, name='delete-product'),
     path('order/', views.order, name='dashboard-order'),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from .models import Product
+from .forms import ProductForm
 
 
 @login_required(login_url='user-login')
@@ -13,8 +15,32 @@ def staff(request):
 
 @login_required(login_url='user-login')
 def product(request):
-    return render(request, 'dashboard/product.html')
+    items = Product.objects.all()
+    form = ProductForm()
+
+    if request.method == 'POST':
+        form = form = ProductForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('dashboard-product')
+    else:
+        form = form = ProductForm()
+    context = {
+        'items': items,
+        'form': form
+    }
+    return render(request, 'dashboard/product.html', context)
 
 @login_required(login_url='user-login')
 def order(request):
     return render(request, 'dashboard/order.html')
+
+
+def delete_product(request, pk):
+    item = Product.objects.get(id=pk)
+    
+    if request.method == 'POST':
+        item.delete()
+        return redirect("dashboard-product")
+
+    return render(request, 'dashboard/product_delete.html')
