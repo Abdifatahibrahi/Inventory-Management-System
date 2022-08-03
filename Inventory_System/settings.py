5250928f5f20d0e116d3f650065397edc044e4b5
Abdifatah Ibrahim <alsidiq099@gmail.com>
2022-08-03 14:05:01 +0300

    Added forgot password functionalities

--- Inventory_System/settings.py
+++ Inventory_System/settings.py
@@ -147,3 +147,10 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 # LOGIN_URL = 'dashboard-index'
 
 LOGIN_REDIRECT_URL = 'dashboard-index'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'bullebullow@gmail.com'
+EMAIL_HOST_PASSWORD = 'Bulle987612345'
